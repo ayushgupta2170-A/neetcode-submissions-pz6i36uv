fc50dc09dee269adefa654bf81f45e1c5ced99e0
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-04-08 21:59:54 +0530

    Add: concatenation-of-array - submission-5

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+      // int n=nums.size();
+      /* vector<int>ans(2*n);
+       for(int i=0;i<n;i++){
+        ans[i]=nums[i];
+        ans[i+n]=nums[i];
+         }
+         return ans;*/
+         /* for(int i=0;i<n;i++){
+            nums.push_back(nums[i]);
+          }
+          return nums;*/
+
+            
+          int n=nums.size();  
+          vector<int>ans(2*n);
+          copy(nums.begin(),nums.end(),ans.begin());
+          copy(nums.begin(),nums.end(),ans.begin()+n);
+          return ans;
+
+    }
+};
