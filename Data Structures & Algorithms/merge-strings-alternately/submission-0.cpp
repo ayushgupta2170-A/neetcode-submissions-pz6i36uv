49ba7655ca89b2f4e80451a3b58d421bf24d26cb
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-04-16 23:56:38 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string mergeAlternately(string s1, string s2 ) {
+       string result="";
+       int i=0;
+       //takes alag alag 
+       while( i<s1.size() && i<s2.size()){
+        result=result+s1[i];// from s1
+        result=result+s2[i];//from s2
+        i++;
+        }
+        //lastcharacter add krne ke liye 
+        //     2 ways
+        while(i<s1.size()){
+           result+=s1[i];
+           i++;
+        }
+        while(i<s2.size()){
+           result+=s2[i];
+           i++;
+        }
+        return result;
+       
+       
+       
+    }
+};
